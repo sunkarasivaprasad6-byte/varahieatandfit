65eb2d1885a9f0ab1880d6aea73a66f8aa16fb83
sunkarasivaprasad6-byte <sunkarasivaprasad6@gmail.com>
2026-08-27 14:07:59 +0530

    Fix image path for Protein Shakes

--- components/FeaturedFoods.tsx
+++ components/FeaturedFoods.tsx
@@ -14,7 +14,7 @@ const categories = [
     id: "Protein Shakes",
     title: "Protein Shakes",
     items: "2 Items",
-    image:/public/images/protein.jpeg,
+    image:/images/protein.jpeg,
   },
   {
     id: "Salads",
